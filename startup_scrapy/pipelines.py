3a136324c8e2ce1d4f231548aaa2950ae0171099
Artur Husti <artur.husty12@gmail.com>
2020-06-26 23:18:30 +0300

    create spider to crawl urls

--- startup_scrapy/pipelines.py
+++ startup_scrapy/pipelines.py
@@ -1,13 +1,16 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exporters import CsvItemExporter
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class CsvUrlPipeline():
+    def __init__(self):
+        self.file = open('urls.csv', 'wb')
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.start_exporting()
 
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
 
-class StartupScrapyPipeline:
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
---
+++ startup_scrapy/spiders/startup_url_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+import json
+
+from startup_scrapy.items import StartupUrlItem
+
+
+class StartupUrlSpider(scrapy.Spider):
+    name = 'startup_url'
+    api_url = 'https://e27.co/api/startups/?all_fundraising=&pro=0&tab_name=recentlyupdated&start={}&length=1000'
+    start_item = 0
+    start_urls = [api_url.format(start_item)]
+    startup_url = 'https://e27.co/startups/{}'
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'startup_scrapy.pipelines.CsvUrlPipeline': 100,
+        }
+    }
+    
+    def parse(self, response):
+        items = StartupUrlItem()
+        self.logger.info('start scraping')
+        
+        data_json = json.loads(response.text)
+        data = data_json['data'].get('list')
+        if data:
+            for slug in data:
+                items['url'] = self.startup_url.format(slug['slug'])
+                yield items
+            self.start_item += 1000
+            yield scrapy.Request(url=self.api_url.format(self.start_item), callback=self.parse)
